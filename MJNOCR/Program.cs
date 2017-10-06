c3109dec838bd72579247bf55b639141f2da8cbe
Ignacy <ignacy130@gmail.com>
2017-10-06 21:11:27 +0200

    merge texts

--- MJNOCR/Program.cs
+++ MJNOCR/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,23 @@ namespace MJNOCR
 	{
 		static void Main(string[] args)
 		{
+			var mainDir = new DirectoryInfo(@"D:\D\Projekty\NEATCODE\MJN\Data\mjn2016\");
+
+			var dirs = mainDir.GetDirectories();
+
+			foreach (var dir in dirs)
+			{
+				var txtFiles = dir.GetFiles("*.txt").OrderBy(x=>x.Name).ToList();
+				var result = "";
+				foreach (var f in txtFiles)
+				{
+					result += File.ReadAllText(f.FullName);
+				}
+				var s = File.CreateText(Path.Combine(dir.FullName, dir.Name + ".txt"));
+				s.Write(result);
+				s.Close();
+			}
+
 		}
 	}
 }
